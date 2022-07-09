be37423aca728e71408da6afe95cafa40904ce69
taylorsreid <taylorsreid@gmail.com>
2022-07-09 08:44:12 -1000

    10.3 progress

--- module_10/bacchus_create_and_insert.py
+++ module_10/bacchus_create_and_insert.py
@@ -11,6 +11,7 @@ Module 10.3
 #library imports
 import mysql.connector
 from mysql.connector import errorcode
+import json
 
 #credentials configuration
 config = {
@@ -22,44 +23,102 @@ config = {
 }
 
 try:
+
+    #
+    with open("bacchus.json") as bacchus_json:
+        bacchus = json.load(bacchus_json)
+
     #credentials loaded in and connection created
     db = mysql.connector.connect(**config)
     cursor = db.cursor()
 
-    def insertSupply(supply_name, supply_price, vendor_id):
-        cursor.execute(f"INSERT INTO supply(supply_name, supply_price, vendor_id) VALUES({supply_name}, {supply_price}, {vendor_id});")
-
-    def insertVendor(vendor_name):
-        cursor.execute(f"INSERT INTO vendor(vendor_id, vendor_name) VALUES({vendor_name});")
-
-    def insertEmployee(employee_first_name, employee_last_name, employee_role):
-        cursor.exectue(f"INSERT INTO employee(employee_first_name, employee_last_name, employee_role) VALUES({employee_first_name}, {employee_last_name}, {employee_role});")
-
-    def insertSupplyOrders(order_date, promised_delivery_date, actual_delivery_date, order_price, vendor_id):
-        cursor.execute(f"INSERT INTO supply_order(order_date, promised_delivery_date, actual_delivery_date, order_price, vendor_id) VALUES({order_date}, {promised_delivery_date}, {actual_delivery_date}, {order_price}, {vendor_id});")
-
-    
-
-    #creates a table with a list of supplies
-    cursor.execute("CREATE TABLE supply(supply_id INT NOT NULL, supply_name VARCHAR(75) NOT NULL, supply_price DECIMAL, vendor_id INT NOT NULL, PRIMARY KEY(supply_id), CONSTRAINT fk_vendor_id FOREIGN KEY(vendor_id) REFERENCES vendor(vendor_id);")
-
     #creates a table with a list of vendors
-    cursor.execute("CREATE TABLE vendor(vendor_id INT NOT NULL, vendor_name VARCHAR(75) NOT NULL, PRIMARY KEY(vendor_id);")
-
-    #creates a table for supply orders
-    cursor.execute("CREATE TABLE supply_order(order_id INT NOT NULL, order_date DATE, promised_delivery_date DATE NOT NULL, actual_delivery_date DATE, order_price DECIMAL NOT NULL, vendor_id INT NOT NULL, PRIMARY KEY(order_id), CONSTRAINT fk_vendor_id FOREIGN KEY(vendor_id) REFERENCES vendor(vendor_id);")
+    cursor.execute("CREATE TABLE vendor(vendor_id INT NOT NULL, vendor_name VARCHAR(75) NOT NULL, PRIMARY KEY(vendor_id));")
 
     #creates the employee table
-    cursor.execute("CREATE TABLE employee(employee_id INT NOT NULL, employee_first_name VARCHAR(75) NOT NULL, employee_last_name VARCHAR(75) NOT NULL, employee_role VARCHAR(75) NOT NULL, PRIMARY KEY(employee_id);")
+    cursor.execute("CREATE TABLE employee(employee_id INT NOT NULL, employee_first_name VARCHAR(75) NOT NULL, employee_last_name VARCHAR(75) NOT NULL, employee_role VARCHAR(75) NOT NULL, PRIMARY KEY(employee_id));")
     
-    #create
-    cursor.execute("CREATE TABLE ")
+    #creates a table with distributors
+    cursor.execute("CREATE TABLE distributor(distributor_id INT NOT NULL, distributor_name VARCHAR(75) NOT NULL, PRIMARY KEY(distributor_id));")
+
+    #creates a table to hold the LAST MONTH'S sales
+    cursor.execute("CREATE TABLE sales(distributor_id INT NOT NULL, merlot VARCHAR(75) NOT NULL, cabernet VARCHAR(75) NOT NULL, chablis VARCHAR(75) NOT NULL, chardonnay VARCHAR(75) NOT NULL, CONSTRAINT fk_distributor_id FOREIGN KEY(distributor_id) REFERENCES distributor(distributor_id));")
+
+    #creates a table to hold each employees hours each quarter
+    cursor.execute("CREATE TABLE hours(employee_id INT NOT NULL, q1 DECIMAL NOT NULL, q2 DECIMAL NOT NULL, q3 DECIMAL NOT NULL, q4 DECIMAL NOT NULL, CONSTRAINT fk_employee_id FOREIGN KEY(employee_id) REFERENCES employee(employee_id));")
+    
+    #creates a table with a list of supplies
+    cursor.execute("CREATE TABLE supply(supply_id INT NOT NULL, supply_name VARCHAR(75) NOT NULL, supply_price DECIMAL, vendor_id INT NOT NULL, PRIMARY KEY(supply_id), CONSTRAINT fk_supply_vendor_id FOREIGN KEY(vendor_id) REFERENCES vendor(vendor_id));")
+
+    #creates a table for supply orders
+    cursor.execute("CREATE TABLE supply_order(order_date DATE NOT NULL, promised_delivery_date DATE NOT NULL, actual_delivery_date DATE, order_price DECIMAL NOT NULL, vendor_id INT NOT NULL, PRIMARY KEY(order_date), CONSTRAINT fk_order_vendor_id FOREIGN KEY(vendor_id) REFERENCES vendor(vendor_id));")
 
     #time to start filling the tables
 
-    #inserts vendors
     
-        #cursor.execute(f"INSERT INTO vendor(name) VALUES('{vendor}');")
+
+
+    #fills the vendor table from the JSON file
+    i = 0
+    while i < len(bacchus["vendor"]):
+        vendor_id = bacchus["vendor"][i]["vendor_id"]
+        vendor_name = bacchus["vendor"][i]["vendor_name"]
+        cursor.execute(f"INSERT INTO vendor(vendor_id, vendor_name) VALUES({vendor_id}, {vendor_name});")
+        i += 1
+
+    #fills the employee table from the JSON file
+    i = 0
+    while i < len(bacchus["employee"]):
+        employee_id = bacchus["employee"][i]["employee_id"]
+        employee_first_name = bacchus["employee"][i]["employee_first_name"]
+        employee_last_name = bacchus["employee"][i]["employee_last_name"]
+        employee_role = bacchus["employee"][i]["employee_role"]
+        cursor.execute(f"INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_role) VALUES({employee_id}, {employee_first_name}, {employee_last_name}, {employee_role});")
+        i += 1
+
+    #fills the distributor table from the JSON file
+    i = 0
+    while i < len(bacchus["distributor"]):
+        distributor_id = bacchus["distributor"][i]["distributor_id"]
+        distributor_name = bacchus["distributor"][i]["distributor_name"]
+        cursor.execute(f"INSERT INTO distributor(distributor_id, distributor_name) VALUES({distributor_id}, {distributor_name});")
+        i += 1
+
+    #fills the sales table from the JSON file
+    i = 0
+    while i < len(bacchus["sales"]):
+        distributor_id = bacchus["sales"][i]["distributor_id"]
+        merlot = bacchus["sales"][i]["merlot"]
+        cabernet = bacchus["sales"][i]["cabernet"]
+        chablis = bacchus["sales"][i]["chablis"]
+        chardonnay = bacchus["sales"][i]["chardonnay"]
+        cursor.execute(f"INSERT INTO sales(distributor_id, merlot, cabernet, chablis, chardonnay) VALUES({distributor_id}, {merlot}, {cabernet}, {chablis}, {chardonnay});")
+        i += 1
+    
+    #fills the hours table from the JSON file
+
+
+    #fills the supply table from the JSON file
+    i = 0
+    while i < len(bacchus["supply"]):
+        supply_id = bacchus["supply"][i]["supply_id"]
+        supply_name = bacchus["supply"][i]["supply_name"]
+        supply_price = bacchus["supply"][i]["supply_price"]
+        vendor_id = bacchus["supply"][i]["vendor_id"]
+        cursor.execute(f"INSERT INTO supply(supply_id, supply_name, supply_price, vendor_id) VALUES({supply_id}, {supply_name}, {supply_price}, {vendor_id});")
+        i += 1
+
+    #fills the supply order table from the JSON file
+    i = 0
+    while i < len(bacchus["supply_order"]):
+        order_id = bacchus["supply_order"][i]["order_id"]
+        order_date = bacchus["supply_order"][i]["order_date"]
+        promised_delivery_date = bacchus["supply_order"][i]["promised_delivery_date"]
+        actual_delivery_date = bacchus["supply_order"][i]["actual_delivery_date"]
+        order_price = bacchus["supply_order"][i]["order_price"]
+        vendor_id = bacchus["supply_order"][i]["vendor_id"]
+        cursor.execute(f"INSERT INTO supply_order(order_id, order_date, promised_delivery_date, actual_delivery_date, order_price, vendor_id) VALUES({order_id}, {order_date}, {promised_delivery_date}, {actual_delivery_date}, {order_price}, {vendor_id});")
+        i += 1
 
     #commits and closes connections
     db.commit()
--- module_10/test.py
+++ module_10/test.py
@@ -1,3 +1,15 @@
+import mysql.connector
+
+config = {
+    "user": "bacchus_user",
+    "password": "winesnob",
+    "host": "127.0.0.1",
+    "database": "bacchus",
+    "raise_on_warnings": True
+}
+
+db = mysql.connector.connect(**config)
+cursor = db.cursor()
 import json
 
 with open("bacchus.json") as bacchus_json:
@@ -5,7 +17,15 @@ with open("bacchus.json") as bacchus_json:
 
 print(type(bacchus))
 
+def insertEmployee(employee_id, employee_first_name, employee_last_name, employee_role):
+        cursor.execute(f"INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_role) VALUES({employee_first_name}, {employee_last_name}, {employee_role});")
+
 i = 0
 while i < len(bacchus["employee"]):
-    print(bacchus["employee"][i]["employee_first_name"])
+    employee_id = bacchus["employee"][i]["employee_id"]
+    employee_first_name = bacchus["employee"][i]["employee_first_name"]
+    employee_last_name = bacchus["employee"][i]["employee_last_name"]
+    employee_role = bacchus["employee"][i]["employee_role"]
+    insertEmployee(employee_id, employee_first_name, employee_last_name, employee_role)
+
     i += 1
