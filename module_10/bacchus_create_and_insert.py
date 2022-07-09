6a91f8ff833ec909ec6d0a1c33c9051f7e1b4274
taylorsreid <taylorsreid@gmail.com>
2022-07-09 02:18:20 -1000

    10.3 progress

--- module_10/bacchus_create_and_insert.py
+++ module_10/bacchus_create_and_insert.py
@@ -21,20 +21,42 @@ config = {
     "raise_on_warnings": True
 }
 
+suppliers = [
+
+    ["Bob's Bottles and Bungs", "Split Bottle", "750mL Bottle", "1.75L Bottle", "Split Cork", "750mL Cork", "1.75L Cork"],
+
+    ["Sean's Shipping Supplies", "Split Label", "750mL Label", "1.75L Label", "Split Box", "750mL Box", "1.75mL Box"],
+
+    ["Victor's Vinting Vitals"]
+]
+
+#supplies = [[]]
+orders = []
+employees = []
+
 try:
     #credentials loaded in and connection created
     db = mysql.connector.connect(**config)
-    cursor = db.cursor()
-    
-    #creates the employee table
-    cursor.execute("CREATE TABLE employee(employee_id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(75) NOT NULL, last_name VARCHAR(75) NOT NULL, PRIMARY KEY(employee_id)")
+    cursor = db.cursor() 
 
     #creates a table with a list of suppliers
     cursor.execute("CREATE TABLE supplier(supplier_id INT NOT NULL AUTO_INCREMENT, name VARCHAR(75) NOT NULL, PRIMARY KEY(supplier_id)")
 
     #creates a table with a list of supplies
-    cursor.execute("CREATE TABLE supply(supply_id INT NOT NULL AUTO_INCREMENT, name VARCHAR(75) NOT NULL, PRIMARY KEY(supply_id)")
+    cursor.execute("CREATE TABLE supply(supply_id INT NOT NULL AUTO_INCREMENT, name VARCHAR(75) NOT NULL, price DECIMAL, supplier_id INT NOT NULL, PRIMARY KEY(supply_id), CONSTRAINT fk_supplier FOREIGN KEY(supplier_id) REFERENCES supplier(supplier_id)")
+
+    #creates a table for supply orders
+    cursor.execute("CREATE TABLE supply_orders(order_id INT NOT NULL AUTO_INCREMENT, promised_date DATE NOT NULL, actual_date DATE, suppler_id INT NOT NULL, PRIMARY KEY(order_id), CONSTRAINT fk_supplier FOREIGN KEY(supplier_id) REFERENCES supplier(supplier_id)")
+
+    #creates the employee table
+    cursor.execute("CREATE TABLE employee(employee_id INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(75) NOT NULL, last_name VARCHAR(75) NOT NULL, PRIMARY KEY(employee_id)")
     
+    #time to start filling the tables
+
+    #inserts suppliers
+    
+        #cursor.execute(f"INSERT INTO supplier(name) VALUES('{supplier}');")
+
     #commits and closes connections
     db.commit()
     db.close()
