71dea85ce36b096f12752af8aad06c9a221a669e
taylorsreid <taylorsreid@gmail.com>
2022-07-08 16:41:38 -1000

    Module 9.2 Complete

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -21,18 +21,21 @@ try:
     db = mysql.connector.connect(**config)
     cursor = db.cursor()
 
-    #
+    #execute the SQL statements within the parentheses and assigns it to the players variable
     cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id;")
     players = cursor.fetchall()
-    print("-- DISPLAYING PLAYER RECORDS --\n")
+    
+    print("-- DISPLAYING PLAYER RECORDS --")
+    
+    #for loop to output the results of the join query in a readable format
     for player in players:
         print(f"Player ID: {player[0]}")
         print(f"First Name: {player[1]}")
         print(f"Last Name: {player[2]}")
         print(f"Team Name: {player[3]}")
         print()
-    #
 
+    print()
     input("Press any key to continue...")
 
 #in case there's a connection error
